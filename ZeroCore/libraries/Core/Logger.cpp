f0ab6d4b5cd8b5b6a5362f397e2298fb4ba26a27
SharpX12 <46062475+SharpX07@users.noreply.github.com>
2024-07-28 16:14:20 -0300

    Added Billboard (beta),  Added Resource Type, Improve ResourceManager, Added Logger Panel, Improve Editor Camera, Added LogSink, Improve Shader class for Embedded and File textures

--- ZeroCore/libraries/Core/Logger.cpp
+++ ZeroCore/libraries/Core/Logger.cpp
@@ -8,7 +8,13 @@ namespace Zero
 	void Log::initialize()
 	{
 		spdlog::set_pattern("%^[%T] %n(%L): %v %$");
-		s_CoreLogger = spdlog::stdout_color_mt("ZERO");
+
+        s_LogSink = std::make_shared<LogSink>();
+
+        s_CoreLogger = std::make_shared<spdlog::logger>("ZERO", spdlog::sinks_init_list{
+            std::make_shared<spdlog::sinks::stdout_color_sink_mt>(),
+            s_LogSink
+            });
 		s_CoreLogger->set_level(spdlog::level::trace);
 		s_ClientLogger = spdlog::stdout_color_mt("APP");
 		s_ClientLogger->set_level(spdlog::level::trace);
